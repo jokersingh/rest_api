f0a049e9c3749dc50ee30f7313c8f781f601fd56
Bibhuti <jokersingh@gmail.com>
2014-08-17 20:38:54 +0530

    rest controller annotation

--- src/main/java/com/barun/restapi/controller/HomeController.java
+++ src/main/java/com/barun/restapi/controller/HomeController.java
@@ -7,19 +7,19 @@ import java.util.Locale;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.barun.restapi.domain.User;
 
 /**
  * Handles requests for the application home page.
  */
-@Controller
+@RestController
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
